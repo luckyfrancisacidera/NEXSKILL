d4334c7ba52bd62d85498371ba5480403be78cec
@programmingst <lucky.a.acidera@gmail.com>
2026-02-22 16:14:56 +0800

    feat: enhance design architecture

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), ],
-  
+  plugins: [react(), tailwindcss()],
+   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
 })
