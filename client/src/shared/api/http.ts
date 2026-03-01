7380ac9174635622f7d43fbf022ab89d5a291702
@programmingst <lucky.a.acidera@gmail.com>
2026-03-02 01:16:45 +0800

    refactor: http endpoints

--- client/src/shared/api/http.ts
+++ client/src/shared/api/http.ts
@@ -1,6 +1,6 @@
 import axios from '@shared/vendor/axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://localhost:5062';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5062';
 let csrfToken: string | null = null;
 
 export const http = axios.create({
