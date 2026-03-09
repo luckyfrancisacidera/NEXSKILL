2b73f1cebf0aa48b9fb8feec6fe41beb9befc8c0
@programmingst <lucky.a.acidera@gmail.com>
2026-03-10 02:57:29 +0800

    feat : give initial theme provider

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,15 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@app/routes/router';
-import { ThemeProvider } from '@app/providers/ThemeProviders'
+import { ThemeProvider } from '@app/providers/ThemeProvider';
 import { AuthProvider } from '@app/providers/AuthProvider';
 import { ToastProvider } from './providers/ToastProvider';
+
 const App = () => (
   <ThemeProvider>
-  <ToastProvider>
-  <AuthProvider >
-    <RouterProvider router={router} />
-  </AuthProvider>
-  </ToastProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ToastProvider>
   </ThemeProvider>
 );
 
