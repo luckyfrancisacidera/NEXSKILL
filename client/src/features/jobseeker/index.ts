a9cb2c2282d388a52e7d53ef086cf18278e3ea49
@programmingst <lucky.a.acidera@gmail.com>
2026-03-09 23:46:26 +0800

    refactor: jobseeker codebase

--- client/src/features/jobseeker/index.ts
+++ client/src/features/jobseeker/index.ts
@@ -1,3 +1,13 @@
 // File: src/features/jobseeker/index.ts
-export * from './pages/';
-export * from './data';
+export * from "./types/jobseeker.types";
+export * from "./jobseeker.loaders";
+export * from "./jobseeker.actions";
+export { DashboardPage } from "./pages/DashboardPage";
+export { JobsPage } from "./pages/JobsPage";
+export { JobDetailPage } from "./pages/JobDetailPage";
+export { ApplicationsPage } from "./pages/ApplicationPages";
+export { MessagesPage } from "./pages/MessagePage";
+export { ProfilePage } from "./pages/ProfilePage";
+export { SavedJobsPage } from "./pages/SavedJobsPage";
+export { SettingsPage } from "./pages/SettingsPage";
+export * from "./data";
